325b3f79c0cf37d5a0bb0455023161e6a8634679
Ben Holgate <holgateb@hotmail.com>
2022-12-07 12:29:19 -0800

    updated database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,46 +12,25 @@ const initdb = async () =>
     },
   });
 
-export const postDb = async (content) => {
-  console.log('Post to the ase');
-  const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todos', 'readwrite');
-  const store = tx.objectStore('todos');
-  const request = store.add({ todo: content });
-  const result = await request;
-  console.log('🚀 - data saved to the database', result);
-};
-
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   console.log('PUT to the database');
-  const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todo', 'readwrite');
-  const store = tx.objectStore('todos');
-  const request = store.put({ id: id, todo: content });
+  const openDB = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, jate: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
 
 export const getDb = async () => {
   console.log('GET all from the database');
-  const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todos', 'readonly');
-  const store = tx.objectStore('todos');
-  const request = store.getAll();
+  const openDB = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
-};
-
-export const getOneDb = async (id) => {
-  console.log('GET from the database');
-  const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todos', 'readonly');
-  const store = tx.objectStore('todos');
-  const request = store.getAll();
-  const result = await request;
-  console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
